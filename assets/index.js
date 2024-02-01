5657ba3f1b6886ef1f25e1eff15a7a3e32bfaf90
courtneyhook <118090383+courtneyhook@users.noreply.github.com>
2024-01-31 20:39:05 -0600

    created a function to add the ingredient from the input to the unordered list on the DOM

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,23 @@
 var searchedIngredient = document.getElementById("searched-ingredient");
 var submitIngredientEl = document.getElementById("submit-btn");
+var ingredientUnordered = document.getElementById("ingredients-list");
 var ingredientList = [];
 
-function saveToLocalStorage(event) {
-  event.preventDefault();
+function saveToLocalStorage() {
   var item = searchedIngredient.value;
   ingredientList.unshift(item);
+  addToIngredientList();
   searchedIngredient.value = ""; // this can move into another function
   var stringifiedList = JSON.stringify(ingredientList);
   localStorage.setItem("list", stringifiedList);
+}
+
+function addToIngredientList() {
+  var item1 = searchedIngredient.value;
+  console.log(item1);
+  var itemName = document.createElement("li");
+  itemName.textContent = item1;
+  ingredientUnordered.appendChild(itemName);
 }
 
 submitIngredientEl.addEventListener("click", saveToLocalStorage);
