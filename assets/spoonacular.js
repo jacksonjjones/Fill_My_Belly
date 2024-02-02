d466bd163104f83feb17e5f992ecdffff32b717a
IMANI EDWARDS <imedwards720@gmail.com>
2024-02-01 19:05:39 -0800

    Spoonacular api skeleton

---
+++ assets/spoonacular.js
@@ -0,0 +1,45 @@
+var apiKey = 'ff0ab97c408a433aa7dea6ebb48a0161'; // Replace with your actual API key
+
+
+// Function to call the Spoonacular API with ingredients
+async function getRecipesByIngredients(ingredients) {
+    var apiUrl = `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${ingredients}&apiKey=${apiKey}&fillIngredients=true`;
+
+    try {
+        var response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+
+        var data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+    }
+}
+
+// Example usage
+var ingredients = '';
+// Replace with your desired ingredients
+getRecipesByIngredients(ingredients)
+    .then(recipes => {
+        console.log(recipes)
+        if (!recipes.results.length) {
+            return alert("Too Many Ingredients. Try fewer Ingredients!")
+        }
+        for (var recipe of recipes.results) {
+            fetch(`https://api.spoonacular.com/recipes/${recipe.id}/information?apiKey=${apiKey}`).then(
+                function (res) {
+                    return res.json()
+
+                }).then(function (data) {
+                    console.log(data)
+                })
+        }
+
+
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
