0fdcc3914b3075ed5c1008e26ee1209e6344a3ff
ews578 <ewsnider28@yahoo.com>
2024-02-05 17:15:24 -0600

    added java for words API

--- assets/edamam.js
+++ assets/edamam.js
@@ -2,7 +2,7 @@ var key = "edd4b07feefdc7857017edcc48535233"
 var baseURl = "https://api.edamam.com/api/nutrition-data"
 var appId = "275bff87"
 
-fetch (`${baseURl}?app_id=${appId}&app_key=${key}&ingr=chocolate&nutrition-type=logging`)
+fetch (`${baseURl}?app_id=${appId}&app_key=${key}&ingr=beef&nutrition-type=logging`)
 .then(function(res){
     return res.json()
 })
